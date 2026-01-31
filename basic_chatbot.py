d9791d061b0090432996210a58874eaef303bd0e
rithwik <rithwikvasa@gmail.com>
2026-01-31 18:00:39 +0530

    Ram is added to the llm model

--- basic_chatbot.py
+++ basic_chatbot.py
@@ -5,6 +5,8 @@ from langchain_openai import ChatOpenAI
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
 import os
+from langgraph.checkpoint.memory import MemorySaver  #this save  and update the state after every conversation complted and give total conversion to the model in next invoke with query and store in RAM
+
 
 
 """BaseMessage is the parent class for all conversation messages
@@ -35,30 +37,34 @@ def chat_node(state: ChatState):
     return {"messages": [response]}
 
 # GRAPH
-
+checkpointer=MemorySaver()
 graph = StateGraph(ChatState)
 graph.add_node("chat_node", chat_node)
 graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
-workflow = graph.compile()
+workflow = graph.compile(checkpointer=checkpointer)
 
 # RUN
 
 if __name__ == "__main__":
-
+    thread_id='1'
     while True:
 
         human_message = input("Please ask the question to the chat model:\n")
         if human_message.strip().lower() in ["exit","quit","bye"]:
             break
+        config={"configurable":{"thread_id": thread_id}}
         result = workflow.invoke(
         {
             "messages": [HumanMessage(content=human_message)]
-        }
+        },
+        config=config
         )
         print("\n=== AI RESPONSE ===\n")
         print(result["messages"][-1].content)
     print("THANK-YOU")
 
-    
+"""
+workflow.get_state(config=config) you will get all the conversations
+"""
